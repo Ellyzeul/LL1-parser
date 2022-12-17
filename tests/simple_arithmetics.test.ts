6cd863309d473758ac82251be1f0239d01bb8c31
Ellyzeul <gabrielaug.240@gmail.com>
2022-12-17 16:23:49 -0300

    test: better test description

--- tests/simple_arithmetics.test.ts
+++ tests/simple_arithmetics.test.ts
@@ -2,6 +2,25 @@ import { describe, it, expect } from "@jest/globals"
 import generateParser from "../src/generateParser";
 import parseToken from "../src/parseToken";
 
+/**
+ * Original unfactored grammar
+ * 
+ * rexpr     -> rexpr + rterm | rterm
+ * rterm     -> rterm rfactor | rfactor
+ * rfactor   -> rfactor * | rprimary
+ * rprimary  -> a | b
+ * 
+ * Factored grammar
+ * 
+ * rexpr     -> rterm rexpr'
+ * rexpr'    -> + rterm rexpr' | ε
+ * rterm     -> rfactor rterm'
+ * rterm'    -> rfactor rterm' | ε
+ * rfactor   -> rprimary rfactor'
+ * rfactor'  -> * rfactor' | ε
+ * rprimary  -> a | b
+ */
+
 const parser = generateParser({
   headRule: '<rexpr>',
   '<rexpr>':                  '<rterm> <rexpr*>',
@@ -21,11 +40,11 @@ const testParse = (token: string, has_errors: boolean = false, parsing_ended: bo
   expect(result.errors.length).toBe(errorsLength)
 }
 
-describe('Parsing the following input: a + b * b', () => {
-  it('should parse: <a> + b * b', () => testParse('a'))
-  it('should parse: a <+> b * b', () => testParse('+'))
-  it('should parse: a + <b> * b', () => testParse('b'))
-  it('should parse: a + b <*> b', () => testParse('*'))
-  it('should parse: a + b * <b>', () => testParse('b'))
+describe('Parsing the following input: a+b*b', () => {
+  it('should parse: <a> + b * b $', () => testParse('a'))
+  it('should parse: a <+> b * b $', () => testParse('+'))
+  it('should parse: a + <b> * b $', () => testParse('b'))
+  it('should parse: a + b <*> b $', () => testParse('*'))
+  it('should parse: a + b * <b> $', () => testParse('b'))
   it('should parse: a + b * b <$>', () => testParse('$', false, true, 0))
 })
